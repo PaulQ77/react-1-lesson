9854b4b5a4d4f7df4849b412567cca7168baf571
Paul Quiroz <pquiroz1977@gmail.com>
2018-03-16 11:59:39 -0700

    Step 3

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        Picture: 
+        <input />
+
+        Name: 
+        <input />
+
+        <button> Add friend</button>
         
       </div>
     );
